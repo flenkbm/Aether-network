e5d9282e1c05986a2e32f09fa964afbbd26db7b2
flenkbm <flenkbm@gmail.com>
2026-07-20 21:07:01 +0300

    Update index-data-loader.js

--- js/index-data-loader.js
+++ js/index-data-loader.js
@@ -6,31 +6,33 @@ function gotoLogin() {
     }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  var raw_usr = localStorage.getItem("Aether-user");
-  console.log(raw_usr);
-  if (!raw_usr) {
+function loadData(){
+  var raw_userlogdata = localStorage.getItem("Aether-user");
+  console.log(raw_userlogdata);
+  if (!raw_userlogdata) {
     gotoLogin();
     return;
   }
-  const user = JSON.parse(raw_usr);
-  console.log(user);
+  const userlogdata = JSON.parse(raw_userlogdata);
+  console.log(userlogdata);
   const now = Date.now();
   console.log(`Now: ${now}`);
   const timeout = 12 * 60 * 60 * 1000; // 12hrs timeout (times minutes, seconds, milliseconds)
   //const timeout = 60 * 1000; // 1min test timeout
-  if (now - user["timestamp"] >= timeout) {
+  if (now - userlogdata["timestamp"] >= timeout) {
     localStorage.removeItem("Aether-user");
     gotoLogin();
     return;
   }
   //Data loading from server part
   // (this is a "demo" version of data loading which may change, but probably won't)
-  window.fetch(`/API/${user["UUID"]}.json`)
+  window.fetch(`/API/${userlogdata["UUID"]}.json`)
     .then((response) => {
       return response.json();
     })
     .then((json) => {
       console.log(json);//test thing
     });
-});
+}
+
+document.addEventListener('DOMContentLoaded', loadData);
