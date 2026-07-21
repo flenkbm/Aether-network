d2dbb10a36943b193f5178cbf0fd252088d35734
flenkbm <flenkbm@gmail.com>
2026-07-21 13:20:03 +0300

    blahblahblah

--- js/index-data-loader.js
+++ js/index-data-loader.js
@@ -6,14 +6,14 @@ function gotoLogin() {
     }
 }
 
-function loadData(){
+function loadUserData() {
   var raw_userlogdata = localStorage.getItem("Aether-user");
   console.log(raw_userlogdata);
   if (!raw_userlogdata) {
     gotoLogin();
     return;
   }
-  const userlogdata = JSON.parse(raw_userlogdata);
+  userlogdata = JSON.parse(raw_userlogdata);
   console.log(userlogdata);
   const now = Date.now();
   console.log(`Now: ${now}`);
@@ -26,13 +26,18 @@ function loadData(){
   }
   //Data loading from server part
   // (this is a "demo" version of data loading which may change, but probably won't)
-  window.fetch(`/API/${userlogdata["UUID"]}.json`)
+  window.fetch(`http://88.210.12.42/API/opnefiles/${userlogdata["UUID"]}.json`)
     .then((response) => {
       return response.json();
     })
     .then((json) => {
-      console.log(json);//test thing
+      userdata = json;
+      console.log(userdata);//test thing
     });
 }
 
-document.addEventListener('DOMContentLoaded', loadData);
+function loadAppData() {
+  return;
+}
+
+document.addEventListener('DOMContentLoaded', () => {loadAppData();loadUserData();console.log(userdata);});
